c538ce5b11ac44c7934be57a94b5b9aa126c5cd1
abdelhamidkb <hamidtech480@gmail.com>
2020-05-12 03:37:10 +0100

    Updates

---
+++ precache-manifest.c4a7ab66de0e65f4b025b2269c72c505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bc72bbb44678375b692",
+    "url": "/robofriends/static/css/main.d4cb399a.chunk.css"
+  },
+  {
+    "revision": "2bc72bbb44678375b692",
+    "url": "/robofriends/static/js/main.0319763b.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "802643f721fa51078946",
+    "url": "/robofriends/static/css/2.7ec11cb4.chunk.css"
+  },
+  {
+    "revision": "802643f721fa51078946",
+    "url": "/robofriends/static/js/2.b245404d.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "17414f4cacf80683fb237b7b6684074d",
+    "url": "/robofriends/index.html"
+  }
+];
